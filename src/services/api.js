6ff263a372ea878f6e2af04dec653d1de60a7ea6
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2020-07-16 10:14:20 -0300

    first deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const Api = axios.create({baseURL: "http://localhost:3001"})
+const Api = axios.create({baseURL: process.env.REACT_APP_BASE_API})
 // toda cjamada que eu fizer usando o axios injetará essa URL antes
 //ai se essa url mudar, só preciso mudar aqui
 
